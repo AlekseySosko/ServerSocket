7762266b851e9c66b4602b81a07a8019f8934d75
Sosko Aleksey <sosko.aleksey@gmail.com>
2022-05-02 13:30:06 +0700

    Задание 1 / Доработка

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,15 +1,25 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        try (Socket socket = new Socket("localHost", 8786)) {
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-            Scanner scanner = new Scanner(socket.getInputStream());
-            writer.println("client");
+        try (Socket clientSocket = new Socket("localHost", 8786);
+             PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+             Scanner scanner = new Scanner(clientSocket.getInputStream());
+             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
             System.out.println(scanner.nextLine());
+
+            writer.println(reader.readLine());
+
+            System.out.println(scanner.nextLine());
+
+            writer.println(reader.readLine());
+
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Server started!");
 
-        try (ServerSocket serverSocket = new ServerSocket(8786)) {
-            Socket socket = serverSocket.accept();
-            Scanner scanner = new Scanner(socket.getInputStream());
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+        try (ServerSocket serverSocket = new ServerSocket(8786);
+             Socket clientSocket = serverSocket.accept();
+             Scanner scanner = new Scanner(clientSocket.getInputStream());
+             PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)) {
 
             System.out.println("New connection accepted");
 
-            final String name = scanner.nextLine();
+            writer.println("Write your name");
 
-            writer.println(String.format("Hi %s, your port is %d", name, socket.getPort()));;
+            String name = scanner.nextLine();
+
+            System.out.println("Name client: " + name);
+
+            writer.println("Are you child? (yes/no)");
+
+            String answerOne = scanner.nextLine();
+
+            if (answerOne.equals("yes")) {
+                System.out.println("Welcome to the kids area, " + name + " Let's play!");
+            } else if (answerOne.equals("no")) {
+                System.out.println("Welcome to the adult zone, " + name + " Have a good rest, or a good working day!");
+            }
         }
 
     }
